c8ee74131cc2a3018b600cad3ce1a27024828cb3
seragsabrysakr <srag.sabry@gmail.com>
2025-01-25 01:37:11 +0200

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:example/constants.dart';
+import 'package:example/zatca_constants.dart';
 import 'package:flutter/material.dart';
  import 'package:zatca_2_invoice/zatca_2_invoice_generator.dart';
 
